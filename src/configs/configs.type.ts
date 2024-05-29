eeb5a4ad7a95b5d0ccbd75f2a10106a8faee3f13
OlegMatus <129312840+OlegMatus@users.noreply.github.com>
2024-05-30 00:59:27 +0300

    lesson4

--- src/configs/configs.type.ts
+++ src/configs/configs.type.ts
@@ -1,25 +1,25 @@
 export type Config = {
   app: AppConfig;
-  database:    DatabaseConfig;
+  database: DatabaseConfig;
   redis: RedisConfig;
 };
 
 export type AppConfig = {
-  port:      number;
+  port : number;
   host: string;
 };
 
 export type DatabaseConfig = {
-  host:      string;
+  host: string;
 
   port: number;
-  user: string;
+  user: string
   password: string;
   dbName: string;
 };
 
 export type RedisConfig = {
-  port: number;
+  port: number
   host: string;
   password: string;
 };
